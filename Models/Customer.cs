55e7b3e09f40babe42f88766137f50975979550b
DerekPeacock <derek.peacock@yahoo.com>
2019-12-02 10:29:24 +0000

    checked oout and modified customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -12,20 +12,37 @@ namespace TwinSkinLondon.Models
     public class Customer
     {
         public int CustomerID { get; set; }
+
         [Required]
         [StringLength(50, ErrorMessage = "FirstName cannot be longer than 50 characters.")]
         [Display(Name="First Name")]
         public string FirstName { get; set; }
+
         [Required]
         [StringLength(50, ErrorMessage = "LastName cannot be longer than 50 characters.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         [Required]
-        [StringLength(50)]
+        [StringLength(50), DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
+        [StringLength(20), DataType(DataType.PhoneNumber)]
         public string ContactNumber { get; set; }
-        [Required]
-        private string Password { get; set; }
+
+        public int PaymentCardID { get; set; }
+        public int AddressID { get; set; }
+        // Navigation Properties
+
+        public virtual PaymentCard PaymentCard { get; set; }
+
+        public virtual Address Address { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+
+        public virtual ICollection<Message> Messages { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
 
     }
 }
