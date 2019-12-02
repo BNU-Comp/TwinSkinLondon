d6ed0753c67210a888d8496b192017278589a15a
DerekPeacock <derek.peacock@yahoo.com>
2019-12-02 11:27:24 +0000

    corrected OrderItem

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,19 @@ namespace TwinSkinLondon.Models
     public class OrderItem
     {
         public int OrderID { get; set; }
+
         public int ProductID { get; set; }
+
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C0}")]
         public decimal Price { get; set; }
+
         public int Quantity { get; set; }
+
+        // Navigation
+
+        public Order Order { get; set; }
+        public Product Product { get; set; }
 
     }
 }
